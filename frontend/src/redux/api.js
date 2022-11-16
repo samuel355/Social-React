256ea21d016b6ce44eb3b0453fe97d0dc3f90e7a
Samuel Osei Adu <asamuel355@yahoo.com>
2022-11-16 19:37:57 +0000

    tour reducer setup

--- frontend/src/redux/api.js
+++ frontend/src/redux/api.js
@@ -3,8 +3,12 @@ import axios from 'axios'
 const API = axios.create({
     baseURL: "http://localhost:8000"
 })
+//sign in 
 export const signIn = (formData) => API.post('/login', formData);
-
+//sign up
 export const signUp = (formData) => API.post('/signup', formData)
-
+//google signin
 export const googleSignIn = (result) => API.post('/googleSignIn', result)
+
+//Create Tour
+export const createTour = (tourData) => API.post('/create', tourData)
