cb380a5a69ad17dfceeef6364637649f75a48d70
Samuel Osei Adu <asamuel355@yahoo.com>
2022-11-16 11:16:32 +0000

    redux setup

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import {MDBCard, MDBCardBody, MDBInput, MDBCardFooter, MDBValidation, MDBBtn, MDBIcon, MDBSpinner } from 'mdb-react-ui-kit'
+import {MDBCard, MDBCardBody, MDBInput, MDBCardFooter, MDBValidation, MDBBtn, MDBIcon, MDBSpinner, MDBFooter } from 'mdb-react-ui-kit'
 import {Link} from 'react-router-dom'
 
 const initialState = {
@@ -10,10 +10,66 @@ const initialState = {
 const Login = () => {
     const [formValue, setFormValue] = useState(initialState)
     const {email, password} = formValue
-    
+
+    const handleSubmit = (e) => {
+        e.preventDefault() 
+    }
+
+    const onInputChange = (e) => {
+        e.preventDefault() 
+        let {name, value} = e.target;
+        setFormValue({...formValue, [name]: value})
+    }
+
   return (
-    <div>Login</div>
-  )
+    <div style={{margin: 'auto', padding: '15px', maxWidth: '500px', alignContent: 'center', marginTop: '120px'}}>
+        <MDBCard alignment='center' style={{padding: '15px'}}>
+            <MDBIcon fas icon="user-circle" className='fa-2x' />
+            <h4>Log In</h4>
+            <MDBCardBody>
+                <MDBValidation onSubmit={handleSubmit} noValidate className='row g-3'>
+                    <div className="col-md-12">
+                        <MDBInput 
+                          label="Email" 
+                          type='email' 
+                          value={email} 
+                          name="email" 
+                          onChange ={onInputChange} 
+                          required 
+                          invalid 
+                          validation="Please provide your email" 
+                        />
+                    </div>
+
+                    <div className="col-md-12">
+                        <MDBInput 
+                          label="Password" 
+                          type='password' 
+                          value={password} 
+                          name="password" 
+                          onChange ={onInputChange} 
+                          required 
+                          invalid 
+                          validation="Please provide your password" 
+                        />
+                    </div>
+                    <div className="col-12">
+                        <MDBBtn style={{width: "100%"}} className="mt-2">
+                            Login
+                        </MDBBtn>
+                    </div>
+                        
+                </MDBValidation>
+            </MDBCardBody>
+
+            <MDBFooter>
+                <Link to={"/register"}>
+                    <p>Don't have an account ? Sign up </p>
+                </Link>
+            </MDBFooter>
+        </MDBCard>
+    </div>
+  );
 }
 
 export default Login
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {MDBCard, MDBCardBody, MDBInput, MDBCardFooter, MDBValidation, MDBBtn, MDBIcon, MDBSpinner } from 'mdb-react-ui-kit'
+import {Link} from 'react-router-dom'
+
+const initialState = {
+  email: "",
+  password: "",
+}
 
 const Register = () => {
+  const [formValue, setFormValue] = useState(initialState)
+  const {email, password} = formValue
+
+  const handleSubmit = (e) => {
+      e.preventDefault() 
+  }
+  const onInputChange = () => {
+
+  }
   return (
-    <div>Register</div>
+    <div style={{margin: 'auto', padding: '15px', maxWidth: '500px', alignContent: 'center', marginTop: '120px'}}>
+        <MDBCard alignment='center' style={{padding: '15px'}}>
+            <MDBIcon fas icon="user-circle" className='fa-2x' />
+            <h4>Log In</h4>
+            <MDBCardBody>
+                <MDBValidation onSubmit={handleSubmit} noValidate className='row g-3'>
+                    <div className="col-md-12">
+                        <MDBInput 
+                          label="Email" 
+                          type='email' 
+                          value={email} 
+                          name="email" 
+                          onChange ={onInputChange} 
+                          required 
+                          invalid 
+                          validation="Please provide your email" 
+                        />
+                    </div>
+
+                    <div className="col-md-12">
+                        <MDBInput 
+                          label="Password" 
+                          type='password' 
+                          value={password} 
+                          name="password" 
+                          onChange ={onInputChange} 
+                          required 
+                          invalid 
+                          validation="Please provide your password" 
+                        />
+                    </div>
+                        
+                </MDBValidation>
+            </MDBCardBody>
+        </MDBCard>
+    </div>
   )
 }
 
